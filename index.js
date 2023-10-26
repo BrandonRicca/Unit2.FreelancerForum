0beda1ea7c8dcee72ff086bd1ff6eb4a71697db9
BrandonRicca <tulshibrandon@gmail.com>
2023-10-26 14:50:32 -0400

    Created freelancers object array

---
+++ index.js
@@ -0,0 +1,15 @@
+let averagePrice = document.getElementById("average")
+averagePrice.innerHTML = "The average starting price is"
+
+const freelancers = [
+  {name: "Denji", occupation: "hunter", price: 150},
+  {name: "Luke", occupation: "cook", price: 30},
+  {name: "Clare", occupation: "bounty hunter", price: 2000},
+  {name: "Paul", occupation: "plumber", price: 25},
+  {name: "Kyle", occupation: "programmer", price: 50},
+  {name: "Samwise Gamgee", occupation: "gardener", price: 20},
+  {name: "Harald Hardrada", occupation: "viking", price: 1000},
+  {name: "Johnny", occupation: "painter", price: 15},
+  {name: "Shirou Emiya", occupation: "sword maker", price: "Free"},
+  {name: "Walter White", occupation: "chemist", price: 400}
+];
